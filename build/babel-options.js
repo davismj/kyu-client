3b52ad56e2014d20dc0ad9f9c45a79e8a7e4925e
Matthew James Davis <davis.matthewjames@gmail.com>
2015-05-16 23:43:16 -0400

    added ai forreal

---
+++ build/babel-options.js
@@ -0,0 +1,13 @@
+// documentation for these options can be
+// found at https://6to5.org/docs/usage/options/
+module.exports = {
+  modules: 'system',
+  moduleIds: false,
+  comments: false,
+  compact: false,
+  stage:2,
+  optional: [
+    "es7.decorators",
+    "es7.classProperties"
+  ]
+};
